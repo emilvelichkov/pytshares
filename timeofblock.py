7d36d61f8df934ff76113882a5ad6719b027958d
Fabian Schuh <mail@xeroc.org>
2014-09-28 16:34:44 +0200

    timeofblock.py

---
+++ timeofblock.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from datetime import datetime,timedelta
+import sys
+
+genesisTimeStamp = "20140719T031850"
+genesisTime = datetime.strptime("20140719T031850","%Y%m%dT%H%M%S")
+confirmationTime = timedelta( seconds=10 )
+
+if __name__ == "__main__":
+ blockNum = sys.argv[ 1 ]
+ print(str(genesisTime+confirmationTime*int( blockNum ))+" UTC")
