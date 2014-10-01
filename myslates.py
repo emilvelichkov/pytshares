2a28194fceed1de1d83386f1fa72e4e3c5bddde4
Fabian Schuh <mail@xeroc.org>
2014-10-01 18:38:19 +0200

    new slates

--- myslates.py
+++ myslates.py
@@ -8,18 +8,21 @@ trusted = [
 "angel.bitdelegate",
 "b.delegate.charity",
 "bdnoble",
+"bitcoiners",
 "bitsuperlab.gentso",
+"bts.coin",
+"chinese",
 "clout-delegate1",
-"d.dacsun",
-"daslab",
+"del.coinhoarder",
 "dele-puppy",
 "delegate.baozi",
 "delegate.charity",
-"delegate.coinhoarder",
 "delegate.jabbajabba",
 "delegate.liondani",
+"delegate.nathanhourt.com",
 "delegate.svk31",
-"delegate1-galt",
+"delegate.xeldal",
+"delegate1.john-galt",
 "forum.lottocharity",
 "happyshares",
 "luckybit",
@@ -30,15 +33,40 @@ trusted = [
 "spartako",
 "testz",
 "titan.crazybit",
+]
+
+untrusted = [ 
+"anchor.crazybit",
+"bitsharesx-delegate",
+"cny.bts500",
+"crazybit",
+"d.dacsun",
+"daslab",
+"dc-delegate",
+"delegate.adam",
 "delegate.nathanhourt.com",
-"bitcoiners",
-"chinese",
-"bts.coin",
+"delegate2.adam",
+"delegated-proof-of-steak",
+"do-you-even-dpos",
+"dpos.crazybit",
+"eur.bts500",
+"titan.crazybit",
 ]
 
 if __name__ == "__main__":
  rpc = btsrpcapi(config.url, config.user, config.passwd)
  print rpc.walletopen(config.wallet)
+
+
+ # unapprove ALL
+ r = json.loads(rpc.walletallgetaccounts())
+ accounts = r["result"]
+ for account in accounts :
+  print rpc.unapprovedelegate(account["name"])
+
  print rpc.unlock(config.unlock)
  for d in trusted :
   print rpc.approvedelegate(d)
+ for d in untrusted :
+  print rpc.disapprovedelegate(d)
+
