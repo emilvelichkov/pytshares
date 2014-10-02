f33b7bf4f7b3e629be0137a350cfc38ee3719f6d
Fabian Schuh <mail@xeroc.org>
2014-10-02 09:33:46 +0200

    update

--- timeofblock.py
+++ timeofblock.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 from datetime import datetime,timedelta
+from btsrpcapi import *
+import config
 import sys
 
-genesisTimeStamp = "20140719T031850"
-genesisTime = datetime.strptime("20140719T031850","%Y%m%dT%H%M%S")
 confirmationTime = timedelta( seconds=10 )
 
 if __name__ == "__main__":
- blockNum = int(sys.argv[ 1 ])-1
- print(str(genesisTime+confirmationTime*blockNum)+" UTC")
+     rpc = btsrpcapi(config.url, config.user, config.passwd)
+     print( rpc.getstatus(  ) )
+     status = json.loads(rpc.getstatus())
+     blockhead = status[ "result" ][ "blockchain_head_block_num" ]
+     block = json.loads(rpc.rpcexec({
+       "method": "blockchain_get_block",
+       "params": [blockhead],
+       "jsonrpc": "2.0",
+       "id": 0
+       }))
+     nowtime = datetime.strptime(block[ "result" ][ "timestamp" ],"%Y%m%dT%H%M%S")
+     blockNum = int(sys.argv[ 1 ])
+     print("block %d to appear in <= %s" % (blockNum,str(confirmationTime*(blockNum-blockhead))))
+     print("UTC time: %s" % str(nowtime+confirmationTime*(blockNum-blockhead)))
